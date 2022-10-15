cdf648e4adcc6ffa21aa32c93a045e75f791f9d1
Kushagra <kush12092001@gmail.com>
2022-10-16 00:16:16 +0530

    Profile page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Qrcode from './components/Qrcode';
-import SignIn from './pages/Auth/SignIn';
-import SignUp from './pages/Auth/SignUp';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Qrcode from "./components/Qrcode";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/qrcode" element={<Qrcode />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/qrcode" element={<Qrcode />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { QRCodeCanvas } from "qrcode.react";
+
+const user = {
+  _id: "634afad21f47e2af438e80da",
+  firstName: "Kushagra",
+  lastName: "Wadhwa",
+  password: "kushagra",
+  emailid: "wadhwakushagra01@gmail.com",
+  mobileNum: "9205104557",
+  qr_code: "abcdefgh",
+  isAdmin: true,
+  linkedinProfile: "https://www.linkedin.com/in/kushagra-wadhwa12/",
+  friends: [],
+  company: "Samsung Research & Development, Bangalore",
+  eventsAttended: [],
+  univ_attended: "Delhi Technological University",
+  __v: { $numberInt: "0" },
+};
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div style={{ marginTop: "10%", marginLeft: "10%" }}>
+      <div>
+        {user.firstName} {user.lastName}
+      </div>
+      <div>
+        <QRCodeCanvas
+          id="qrCode"
+          value={user._id}
+          size={200}
+          bgColor={"#ffffff"}
+          level={"H"}
+        />
+      </div>
+      <div>Email: {user.emailid}</div>
+      <div>Mobile Number: {user.mobileNum}</div>
+      <div>LinkedIn Profile: {user.linkedinProfile}</div>
+      <div>Current Company: {user.company}</div>
+      <div>University: {user.univ_attended}</div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
