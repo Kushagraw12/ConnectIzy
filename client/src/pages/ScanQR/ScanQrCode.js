c7ad8ab7d126ac91cb232d88c937cc2979995a8f
Kushagra <kush12092001@gmail.com>
2022-10-16 10:24:48 +0530

    scanner base added

--- client/src/pages/ScanQR/ScanQrCode.js
+++ client/src/pages/ScanQR/ScanQrCode.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import QrReader from "react-qr-scanner";
 
 function ScanQrCode() {
-  return <div>Scanner for QR Codes here</div>;
+  const [res, setRes] = useState("");
+  const handleScan = (data) => {
+    if (data) {
+      setRes(data.text);
+      console.log("RES:", res, "Data: ", data);
+    }
+  };
+  const handleError = (err) => {
+    console.log(err);
+  };
+  const previewStyle = {
+    height: 700,
+    width: 1000,
+    display: "flex",
+    "justify-content": "center",
+  };
+  return (
+    <div>
+      Scanner for QR Codes here
+      <div>
+        <QrReader
+          delay={100}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+        />
+      </div>
+      <div>Result: {res}</div>
+    </div>
+  );
   // take access for camera, scan qr, read the id, send it in post api: /user/getUser with "userID" as body
 }
 
