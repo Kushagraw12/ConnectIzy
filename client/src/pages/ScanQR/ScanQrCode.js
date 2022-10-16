c2437396b246111c23f9f16c87b1eb145ab1724a
Anshika Jain <jainanshika193@gmail.com>
2022-10-16 12:07:34 +0530

    scanqr changes

--- client/src/pages/ScanQR/ScanQrCode.js
+++ client/src/pages/ScanQR/ScanQrCode.js
@@ -1,5 +1,7 @@
+import { Typography } from "@mui/material";
 import React, { useState } from "react";
 import QrReader from "react-qr-scanner";
+import ButtonAppBar from "../../components/Navbar";
 
 function ScanQrCode() {
   const [res, setRes] = useState("");
@@ -13,21 +15,26 @@ function ScanQrCode() {
     console.log(err);
   };
   const previewStyle = {
-    height: 700,
+    height: 400,
     width: 1000,
     display: "flex",
     "justify-content": "center",
   };
   return (
     <div>
-      Scanner for QR Codes here
-      <div>
-        <QrReader
-          delay={100}
-          style={previewStyle}
-          onError={handleError}
-          onScan={handleScan}
-        />
+      <ButtonAppBar />
+      <Typography variant="h3" style={{ textAlign: "center", padding: "2rem" }}>
+        SCAN YOUR QR CODE HERE
+      </Typography>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "auto" }}>
+          <QrReader
+            delay={100}
+            style={previewStyle}
+            onError={handleError}
+            onScan={handleScan}
+          />
+        </div>
       </div>
       <div>Result: {res}</div>
     </div>
