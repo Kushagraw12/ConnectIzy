15d6fffb54fe5acc747be083cec8d8f179561d68
Kushagra Wadhwa <53997603+Kushagraw12@users.noreply.github.com>
2023-07-22 22:54:20 +0530

    Update index.js

--- client/src/redux/api/index.js
+++ client/src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080/" });
+const API = axios.create({ baseURL: "https://connectizy.onrender.com" });
 
 // API.interceptors.request.use((req) => {
 //   if (localStorage.getItem("profile")) {
